38bc16cb430b276aa54e803ee6ac5df88c27c222
Gasijiyo <pys9307@naver.com>
2022-02-17 13:02:56 +0000

    .

---
+++ 01_Print_n_Operator/src/baekjoon/java/No_18108.java
@@ -0,0 +1,14 @@
+package baekjoon.java;
+
+import java.util.Scanner;
+
+public class No_18108 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i-543);
+        sc.close();
+	}
+
+}

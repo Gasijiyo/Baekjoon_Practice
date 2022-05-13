1171c93faf2e6928c035d2e16e4ab07810516fd7
Gasijiyo <pys9307@naver.com>
2022-05-13 21:22:14 +0900

    .

---
+++ 03_Loop_Statement/src/baekjoon/java/_8393.java
@@ -0,0 +1,24 @@
+package baekjoon.java;
+import java.util.Scanner;
+
+public class _8393 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println( (a*a+a)/2 );
+        sc.close();
+        
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        sc.close();
+//        int sum = 0;
+//        for(int i = 1; i <= N ; i++){
+//            sum += i; 
+//            
+//        }       
+//        System.out.println(sum);
+        
+	}
+
+}

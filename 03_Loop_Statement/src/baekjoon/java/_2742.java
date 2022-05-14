69bd2aa465825aa7ca1239d838112c6d02517842
Gasijiyo <pys9307@naver.com>
2022-05-14 23:52:47 +0900

    .

---
+++ 03_Loop_Statement/src/baekjoon/java/_2742.java
@@ -0,0 +1,15 @@
+package baekjoon.java;
+import java.util.*;
+
+public class _2742 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        
+        for (int i = n ; i > 0 ; i--) {
+            System.out.println(i);
+        }
+	}
+
+}

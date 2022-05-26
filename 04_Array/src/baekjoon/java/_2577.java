bd526d1e1cb78d3f2d738e3447aa0c9183cc3f0d
Gasijiyo <pys9307@naver.com>
2022-05-26 16:31:35 +0900

    .

---
+++ 04_Array/src/baekjoon/java/_2577.java
@@ -0,0 +1,30 @@
+package baekjoon.java;
+
+import java.util.*;
+
+public class _2577 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int result = a * b * c;
+		String num = Integer.toString(result);			
+		
+		for (int i = 0; i < 10 ; i++) {
+			int count = 0;
+			for(int j = 0 ; j < num.length(); j++) {
+				if ((num.charAt(j) - '0') == i) {
+//					char 형 문자를 int 형 정수로 변환하기 위해 '0' 을 뺀다
+					count++;
+				}				
+			}
+			System.out.println(count);
+		}
+		
+		
+	}
+
+}

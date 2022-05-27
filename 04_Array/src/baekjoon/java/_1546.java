d5760939539b49773de13d8b35879cffbfa9e4e6
Gasijiyo <pys9307@naver.com>
2022-05-27 11:51:17 +0900

    .

---
+++ 04_Array/src/baekjoon/java/_1546.java
@@ -0,0 +1,36 @@
+package baekjoon.java;
+import java.util.*;
+
+public class _1546 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 시험의 개수, 시험점수 저장할 배열 선언
+		int subject = sc.nextInt();
+		float[] score = new float[subject];
+		
+		// 배열에 점수 입력해 저장
+		for(int i = 0; i < score.length; i++) {
+			score[i] = sc.nextInt();
+		}
+		
+		// 최대값 구하기
+		int m = 0;
+		for (int i = 0; i < score.length; i++) {
+			if (score[i] > m) {
+				m = (int) score[i];
+			}			
+		}
+		
+		// 새로운 점수 저장하고 평균값 구하기
+		float total = 0;
+		for (int j = 0 ; j <score.length ; j++) {
+			score[j] = (score[j] / m * 100); 			
+			total += score[j];
+		}		
+	
+		System.out.println(total / subject);		
+	}
+
+}

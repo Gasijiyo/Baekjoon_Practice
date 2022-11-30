e2554bc9afd08a608852f2d0070fee2587a505ca
Gasijiyo <pys9307@naver.com>
2022-11-30 22:39:24 +0900

    .

---
+++ 06_String/src/baekjoon/java/_10809.java
@@ -0,0 +1,15 @@
+package baekjoon.java;
+
+import java.util.Scanner;
+
+public class _10809 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        char c = sc.nextLine().charAt(0);
+        
+        System.out.println((int)c);
+        sc.close();
+	}
+
+}

c0ae84d24643aa9dfb0f76a6480a473601d57409
Gasijiyo <pys9307@naver.com>
2023-01-29 23:33:27 +0900

    .

--- 06_String/src/baekjoon/java/_10809.java
+++ 06_String/src/baekjoon/java/_10809.java
@@ -6,10 +6,25 @@ public class _10809 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        char c = sc.nextLine().charAt(0);
+		String s = sc.nextLine();
+		sc.close();
+		
+		int[] arr = new int[26];		
+		for (int i = 0 ; i < arr.length ; i++) {
+			arr[i] = -1;
+		}
+		
+		for (int i = 0; i < s.length() ; i++) {
+			char ch = s.charAt(i);
+			if (arr[ch-'a'] == -1) {	// arr원소값이 -1인 경우에만 초기화
+				arr[ch-'a'] = i;
+			}		
+		}	
+		
+		for (int chValue : arr) {
+			System.out.print(chValue + " ");
+		}
         
-        System.out.println((int)c);
-        sc.close();
 	}
 
 }

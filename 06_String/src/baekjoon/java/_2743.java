9d5fcc2ef1120ca473349d78029990fea0a6db9e
Gasijiyo <pys9307@naver.com>
2023-03-04 09:50:19 +0900

    .

---
+++ 06_String/src/baekjoon/java/_2743.java
@@ -0,0 +1,17 @@
+package baekjoon.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2743 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String given = br.readLine();
+		System.out.println(given.length());
+		br.close();
+	}
+
+}
